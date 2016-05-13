e493d62d057e5892eabfc2894c763425ab2ae0de
gapillan <gapillan@my.centennailcollege.ca>
2016-05-13 10:43:53 -0400

    Initial Commit

---
+++ COMP123_S2016_Lesson1B/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* 
+ * Author: Pamela Apillanes 
+ * Date Created: May 13th, 2016
+ * Date Modified: May 13th, 2016
+ * Student ID: 300867201  
+ * 
+ * Description: This program demonstrates GitHub and version control 
+ * best practices.  We also explore Unit Testing 
+ * 
+ * Version: 0.1 - Initial Version
+ * Check Youtube Chanel: Part 3 
+ */
+
+namespace COMP123_S2016_Lesson1B
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
